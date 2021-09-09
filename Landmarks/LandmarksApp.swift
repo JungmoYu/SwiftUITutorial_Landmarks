576f8a73d07de3c3ee953e3e78e33a682de485f9
유정모 <zzoo789@naver.com>
2021-09-09 23:31:06 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jungmo Yu on 2021/09/09.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
